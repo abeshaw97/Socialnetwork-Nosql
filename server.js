6dd817608155cf3e8eb6519ea7b215494ab3b82e
abeshaw97 <shawabra@msu.edu>
2022-07-02 19:10:38 -0400

    update routes and moved server.js into the root folder

---
+++ server.js
@@ -0,0 +1,26 @@
+// Require express and mongoose
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// Connects mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Logs the mongoose queries
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(` ************** Connected on localhost:${PORT} ************* `));
+
