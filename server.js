2c5828a28d6dcd6490477b7bec057cdbecd3c5f2
abeshaw97 <shawabra@msu.edu>
2022-07-03 12:13:20 -0400

    updated ignore files and server.js

--- server.js
+++ server.js
@@ -8,12 +8,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
-
 app.use(require('./routes'));
 
 // Connects mongoose
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api',
+ {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -22,5 +21,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-
 // Logs the mongoose queries
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(` ************** Connected on localhost:${PORT} ************* `));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
 
